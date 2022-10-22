77ec3bccf746e5534844b568bb3edf654b1f6a93
nowadraco <nowadraco@gmail.com>
2022-10-22 02:53:16 -0300

    aula 3-3

--- aula-3-3.js
+++ aula-3-3.js
@@ -1,2 +1,12 @@
-const precoetiqueta = 100;
-const teste = 1
+const precoEtiqueta = 100;
+const formaDePagamento = 4;
+
+if(formaDePagamento === 1){
+   console.log( precoEtiqueta - (precoEtiqueta * 0.10));
+}else if(formaDePagamento === 2){
+    console.log(precoEtiqueta - (precoEtiqueta * 0.15));  
+}else if(formaDePagamento === 3){
+    console.log(precoEtiqueta);
+}else {
+    console.log(precoEtiqueta + (precoEtiqueta * 0.10));
+}
