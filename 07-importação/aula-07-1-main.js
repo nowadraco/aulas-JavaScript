b0fb8c843dce212808b7c89a78a1d0704419879f
nowadraco <Nowadraco@gmail.com>
2022-11-01 00:58:59 +0000

    usando a importação

--- 07-importação/aula-07-1-main.js
+++ 07-importação/aula-07-1-main.js
@@ -1,3 +1,11 @@
 const {gets, print} = require('./aula-07-1-funcoes-auxiliares');
 
-print(gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numerosSorteado = gets();
+    numerosSorteados.push(numerosSorteado);
+    
+}
+
+print(numerosSorteados)
