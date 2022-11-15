b377c7bfc5ed2989dfee765db69cb86fc7a72a40
nowadraco@gmail.com <nowadraco@gmail.com>
2022-11-15 15:10:18 -0300

    testando

--- 09-aula-criando-layout-pokedex/assets/js/main.js
+++ 09-aula-criando-layout-pokedex/assets/js/main.js
@@ -1,6 +1,6 @@
 
-function convertPokemonTypestoli(pokemontypes){
-    return pokemontypes.map((typesSlot) => `<li class="type">${typesSlot.type.name}</li>`)
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 }
 
 function convertPokemonToLi(pokemon) {
@@ -9,8 +9,7 @@ function convertPokemonToLi(pokemon) {
                     <span class="number">#${pokemon.order}</span>
                     <span class="name">${pokemon.name}</span>
                     <div class="detail">
-                        <ol class="types">
-                            ${convertPokemonTypesToLi(pokemon.types).join('')}
+                        <ol class="types">      ${convertPokemonTypesToLi(pokemonTypes).join()}
                         </ol>
 
                         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
