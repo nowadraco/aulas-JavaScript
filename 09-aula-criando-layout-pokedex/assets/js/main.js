ee4fe4dd2142e0c89537639390b80eb94ae180c4
Luiz José SIlvestre <80053389+nowadraco@users.noreply.github.com>
2022-11-15 17:31:28 -0300

    modificando main

--- 09-aula-criando-layout-pokedex/assets/js/main.js
+++ 09-aula-criando-layout-pokedex/assets/js/main.js
@@ -1,18 +1,15 @@
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-                    <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
                     <div class="detail">
-                        <ol class="types">      ${convertPokemonTypesToLi(pokemonTypes).join()}
+                        <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                         </ol>
 
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
 
                     </div>                
                 </li>
